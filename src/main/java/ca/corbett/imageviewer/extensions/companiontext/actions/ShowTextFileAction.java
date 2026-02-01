828b01646a287a8f0aa25eccbe8a5c6120c54d3d
Steve Corbett <steve@corbett.ca>
2026-01-31 22:34:56 -0700

    Issue #8 Add way to launch editor even if no text file

--- src/main/java/ca/corbett/imageviewer/extensions/companiontext/actions/ShowTextFileAction.java
+++ src/main/java/ca/corbett/imageviewer/extensions/companiontext/actions/ShowTextFileAction.java
@@ -4,6 +4,7 @@ import ca.corbett.extras.EnhancedAction;
 import ca.corbett.extras.MessageUtil;
 import ca.corbett.extras.PopupTextDialog;
 import ca.corbett.extras.io.FileSystemUtil;
+import ca.corbett.extras.io.TextFileDetector;
 import ca.corbett.imageviewer.ui.MainWindow;
 
 import java.awt.event.ActionEvent;
@@ -16,6 +17,8 @@ import java.util.logging.Logger;
  * An action to show/edit a companion text file associated with an image.
  * The text file to be shown/edited is specified when constructing the action,
  * but can be modified later via setTextFile() if the text file is renamed or moved.
+ * Alternatively, you can set the createIfNotPresent option (default false) to
+ * have the action create a new empty text file if it does not already exist.
  *
  * @author <a href="https://github.com/scorbo2">scorbo2</a>
  * @since 3.0.0
@@ -25,10 +28,23 @@ public class ShowTextFileAction extends EnhancedAction {
     private static final Logger log = Logger.getLogger(ShowTextFileAction.class.getName());
     private MessageUtil messageUtil;
     private File textFile;
+    private boolean createIfNotPresent;
+    private boolean fileWasCreated;
 
     public ShowTextFileAction(File textFile) {
         super("View/Edit companion text file");
         this.textFile = textFile;
+        this.createIfNotPresent = false;
+        this.fileWasCreated = false;
+    }
+
+    public boolean isCreateIfNotPresent() {
+        return createIfNotPresent;
+    }
+
+    public ShowTextFileAction setCreateIfNotPresent(boolean createIfNotPresent) {
+        this.createIfNotPresent = createIfNotPresent;
+        return this;
     }
 
     public File getTextFile() {
@@ -38,32 +54,26 @@ public class ShowTextFileAction extends EnhancedAction {
     /**
      * If the associated image file is renamed or moved, you can call this
      * method to update the text file reference.
-     *
-     * @param textFile The new location of our text file.
      */
-    public void setTextFile(File textFile) {
+    public ShowTextFileAction setTextFile(File textFile) {
         this.textFile = textFile;
+        return this;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (textFile == null || !textFile.exists() || !textFile.isFile()) {
-            log.warning("CompanionTextFile: No text file to show.");
-            return;
+        fileWasCreated = false;
+        if (!validateFile()) {
+            return; // error already logged/shown
         }
 
-        String text;
+        String text = getFileContents();
         String title = "Text for " + textFile.getName();
-        try {
-            text = FileSystemUtil.readFileToString(textFile);
-        }
-        catch (IOException ioe) {
-            text = ioe.getMessage() == null ? "" : ioe.getMessage();
-            log.log(Level.SEVERE, "Error reading companion text file: " + textFile.getAbsolutePath(), ioe);
-        }
         PopupTextDialog dialog = new PopupTextDialog(MainWindow.getInstance(), title, text, true);
         dialog.setReadOnly(false);
         dialog.setVisible(true);
+
+        // Save the results if the user okayed the dialog and made changes:
         if (dialog.wasOkayed() && !text.equals(dialog.getText())) {
             try {
                 FileSystemUtil.writeStringToFile(dialog.getText(), textFile);
@@ -72,6 +82,84 @@ public class ShowTextFileAction extends EnhancedAction {
                 getMessageUtil().error("Problem saving companion text file: " + ioe.getMessage(), "Save Error", ioe);
             }
         }
+
+        // Force a refresh if this is a new file:
+        // (this will allow our hyperlink label to get added to the thumb panel)
+        // (I'd rather just surgically do that here, because we know only one thumb panel
+        //  needs updating, but there's no avenue in the parent application to get to it from here, so...)
+        if (dialog.wasOkayed() && fileWasCreated) {
+            MainWindow.getInstance().reload();
+        }
+    }
+
+    /**
+     * Retrieves the file's contents as a string.
+     */
+    private String getFileContents() {
+        try {
+            return FileSystemUtil.readFileToString(textFile);
+        }
+        catch (IOException ioe) {
+            log.log(Level.SEVERE, "Error reading companion text file: " + textFile.getAbsolutePath(), ioe);
+            return ioe.getMessage() == null ? "" : ioe.getMessage();
+        }
+    }
+
+    /**
+     * Invoked internally to check our given textFile for validity.
+     *
+     * @return true if we're good, false if some warning or error has been logged.
+     */
+    private boolean validateFile() {
+        // If we were given garbage input, just log a warning and bail:
+        if (textFile == null) {
+            log.warning("CompanionTextFile: No text file to show.");
+            return false;
+        }
+        if ((!textFile.exists() || !textFile.isFile() || !textFile.canRead()) && !createIfNotPresent) {
+            log.warning("CompanionTextFile: Invalid or unreadable text file: " + textFile.getAbsolutePath());
+            return false;
+        }
+        if (!createIfNotPresent) {
+            try {
+                if (!TextFileDetector.isTextFile(textFile)) {
+                    getMessageUtil().warning("Not a text file",
+                                             "The specified companion text file does not appear to be a text file:\n" +
+                                                 textFile.getAbsolutePath());
+                    return false;
+                }
+            }
+            catch (IOException ioe) {
+                getMessageUtil().error("File error",
+                                       "Problem checking companion text file type: " + ioe.getMessage(), ioe);
+                return false;
+            }
+        }
+
+        // If the file doesn't exist, that may or may not be a problem:
+        if (!textFile.exists()) {
+            // If we were not explicitly told to create it, just inform the user and bail:
+            if (!createIfNotPresent) {
+                getMessageUtil().info("File not found",
+                                      "No companion text file found at:\n" + textFile.getAbsolutePath());
+                return false;
+            }
+
+            // Otherwise, initialize the file with empty contents:
+            try {
+                FileSystemUtil.writeStringToFile("", textFile);
+                fileWasCreated = true; // flag this so we can force a refresh later if the dialog is okayed
+            }
+            catch (IOException ioe) {
+                getMessageUtil().error("File creation error",
+                                       "Problem creating companion text file: " + ioe.getMessage(),
+                                       ioe);
+                return false;
+            }
+        }
+
+        // If we get here, the file is either valid, or we have created it successfully:
+        return true;
     }
 
     private MessageUtil getMessageUtil() {
